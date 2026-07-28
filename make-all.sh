b90d6686fa9457fd001a4b038d063df1cd1d5d75
WR0903 <918981878@qq.com>
2026-07-28 20:02:18 +0800

    cmake

--- make-all.sh
+++ make-all.sh
@@ -11,11 +11,18 @@ fi
 
 BUILD_DIR="build"
 
+# 代码中使用 #include <hiredis/hiredis.h>，但 3rdparty 下目录名为 hiredis-master，
+# 建立软链接 hiredis -> hiredis-master 让头文件路径正确解析（不影响 git 跟踪）。
+if [ ! -e "src/3rdparty/hiredis" ]; then
+    ln -sfn hiredis-master src/3rdparty/hiredis
+fi
+
 if ${clean};then
     rm -rf ${BUILD_DIR}
 else
     mkdir -p ${BUILD_DIR}
     cd ${BUILD_DIR}
     cmake -DCMAKE_BUILD_TYPE=${buildType} ..
-    make -j1
+    # 限制并行作业数，避免 allinone 等合并编译目标内存峰值触发 OOM
+    make -j8
 fi
